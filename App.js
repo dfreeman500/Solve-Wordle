bf0f2b6959a876f5755b9b812ba376497db89fa7
Dan <danielfr500@gmail.com>
2022-04-25 11:42:14 -0400

    clean up of code/comments

--- App.js
+++ App.js
@@ -1,83 +1,9 @@
-
-let letterSetFrequency = {}
-let letterFrequency = {}
-
-function resetFrequencies() {
-    letterSetFrequency = {
-        a: 0,
-        b: 0,
-        c: 0,
-        d: 0,
-        e: 0,
-        f: 0,
-        g: 0,
-        h: 0,
-        i: 0,
-        j: 0,
-        k: 0,
-        l: 0,
-        m: 0,
-        n: 0,
-        o: 0,
-        p: 0,
-        q: 0,
-        r: 0,
-        s: 0,
-        t: 0,
-        u: 0,
-        v: 0,
-        w: 0,
-        x: 0,
-        y: 0,
-        z: 0
-
-    }
-
-
-    letterFrequency = {
-        a: 0,
-        b: 0,
-        c: 0,
-        d: 0,
-        e: 0,
-        f: 0,
-        g: 0,
-        h: 0,
-        i: 0,
-        j: 0,
-        k: 0,
-        l: 0,
-        m: 0,
-        n: 0,
-        o: 0,
-        p: 0,
-        q: 0,
-        r: 0,
-        s: 0,
-        t: 0,
-        u: 0,
-        v: 0,
-        w: 0,
-        x: 0,
-        y: 0,
-        z: 0
-
-    }
-}
-resetFrequencies()
-
-let howManyTimes = 0;
-
-
 function findSetOfLetters(currentWordList) {
     for (let i = 0; i < currentWordList.length; i++) {
-        //console.log(currentWordList[i])
         let tempSet = new Set(currentWordList[i])
         tempSet.forEach(item => {
             letterSetFrequency[item] += 1
-            // console.log(item)
         })
-        // howManyTimes += 1;
 
     }
 }
@@ -85,49 +11,25 @@ function findSetOfLetters(currentWordList) {
 let errorWords = ""
 function findLetterDistribution(currentWordList) {
     for (let i = 0; i < currentWordList.length; i++) {
-        // console.log(currentWordList[i])
         for (let j = 0; j < currentWordList[i].length; j++) {
             letterFrequency[currentWordList[i][j]] += 1
-            // console.log(currentWordList[i][j])
 
         }
-        // console.log(letterFrequency)
     }
 }
 
 let countWords = 0;
 function findWordScore(currentWordList) {
-    // console.log("I have run", currentWordList)
     for (let object in currentWordList) {
-        // console.log("i have run 2")
-        // console.log(object)
         let tempSet = new Set(object)
-        // console.log(tempSet)
         tempSet.forEach(item => {
             currentWordList[object] += letterSetFrequency[item]
         })
-        // for (let j = 0; j < tempSet.length; j++){
-        //     console.log("hello")
-        //     console.log(object)
-        // wordScore[object]= letterSetFrequency[tempSet[j]]
         countWords += 1;
-        // console.log(wordScore)
-        // console.log(countWords);
     }
-
-
-
 }
 
 
-// for (let i = 0; i < currentWordList.length; i++) {
-//     console.log(currentWordList[i])
-//     let tempSet = new Set(currentWordList[i])
-//     console.log(tempSet)
-//     for (let j = 0; j < tempSet.length; j++){
-//         wordScore[currentWordList[i]]= letterSetFrequency[tempSet[j]]
-//     }
-// }
 function eliminateWrongLetters(currentWord, eliminatedLetters) {
     for (let i = 0; i < eliminatedLetters.length; i++) {
         if (currentWord.includes(eliminatedLetters[i])) {
@@ -136,27 +38,23 @@ function eliminateWrongLetters(currentWord, eliminatedLetters) {
     }
 }
 
-eliminatedLetters = []
+eliminatedLetters = ['i','s','e']
 
 //Letters in the word and in the correct position
 correctLetterCorrectPositionMap = [
-  ['o',1],
-  ['u',2],
-  ['n',3],
-  ['d',4]
+['a',0],
+['r',1]
 
 ]
 
 //These are letters that must be in the word but are in the incorrect position
 let correctLetterWrongPositionMap = [
-    
-]
 
+]
 
 function buildRegexPattern(letter, position, requirement) {
     output = ''
     for (let i = 0; i < 5; i++) {
-        //console.log(i, letter, position)
         if (position == i) {
             if (requirement == "cantHave") {
                 output += "[^" + letter + "]"
@@ -184,8 +82,6 @@ function rightLetterWrongPosition(currentWord, correctLetterWrongPositionMap) {
         //Can't have correct letter in wrong spot
         //t,0 will eliminate 'thorn'
         pattern = buildRegexPattern(correctLetterWrongPositionMap[i][0], correctLetterWrongPositionMap[i][1], "cantHave")
-        console.log(new RegExp(pattern), currentWord)
-        console.log(currentWord.match(new RegExp(pattern)))
         if (currentWord.match(new RegExp(pattern)) == null) {
             return "exclude";
         }
@@ -195,8 +91,6 @@ function rightLetterWrongPosition(currentWord, correctLetterWrongPositionMap) {
 function rightLetterRightPosition(currentWord, correctLetterCorrectPositionMap) {
     for (let i = 0; i < correctLetterCorrectPositionMap.length; i++) {
         pattern = buildRegexPattern(correctLetterCorrectPositionMap[i][0], correctLetterCorrectPositionMap[i][1], "mustHave")
-        // console.log(new RegExp(pattern), currentWord)
-        // console.log(currentWord.match(new RegExp(pattern)))
         if (currentWord.match(new RegExp(pattern)) == null) {
             return "exclude"
         }
@@ -221,36 +115,15 @@ function includeExcludeWord(currentWordList, eliminatedLetters, correctLetterWro
 }
 
 
-findSetOfLetters(initialWordList);
-// findLetterDistribution(initialWordList);
-
-findWordScore(wordScore);
-
-//regex or loops
-
-
 function createScoringObject(list) {
     const value = 0
-
     const result = list.reduce((acc, list) => (acc[list] = value, acc), {})
-
     return result
 }
 
 
 
 includeExcludeWord(initialWordList, eliminatedLetters, correctLetterWrongPositionMap, correctLetterCorrectPositionMap)
-
-resetFrequencies()
-findSetOfLetters(secondNewWordList);
-
-// createdObject = createScoringObject(secondNewWordList)
-
-// let total = findWordScore(createdObject);
-
-
-//Invalid scoring - ex; contradicting grading
-
 
 
 //Any letter that is definitely in the word no matter if correct location
@@ -279,32 +152,19 @@ function ifAllNewLettersAreWrongHowManyWordsAreELiminated(currentWordList){
                     if(currentWordList[word].includes(tempEliminatedLetters[letter])){
                         numberOfWordsEliminated += 1;     
                         scoringObject[item] = numberOfWordsEliminated;
-
                         break
                     }
                 }
             }
-
-
     }
     return scoringObject;
-
     //if newly added letters are wrong - it will eliminate x from the current list
 }
 
-
-
-
-// fakeWordList=['fined','dinef','dddd','iiiii','fff','nnn']
 mostRecentScore = ifAllNewLettersAreWrongHowManyWordsAreELiminated(secondNewWordList);
 
-sortable = Object.fromEntries(
+sortedList = Object.fromEntries(
     Object.entries(mostRecentScore).sort(([,a],[,b]) => b-a)
 )
 
-// function gradingCheck (){
-//     for (let i=0; i< 
-
-// }
-
-console.log(sortable)
+console.log(sortedList)
